7353f49f9caf5c1f2c822d39d82225e478888a4b
Gustavo Dantas Galote <gusgalote@outlook.com>
2021-05-28 17:31:45 -0300

    OOP Part 2: Constructors

---
+++ src/main/kotlin/parte2_poo/Construtores.kt
@@ -0,0 +1,10 @@
+package parte2_poo
+
+import parte2_poo.classes.CasaComConstrutores
+
+fun main(args: Array<String>) {
+
+    val casa = CasaComConstrutores("Amarela", 2)
+
+    casa.detalhesCasa()
+}
---
+++ src/main/kotlin/parte2_poo/classes/CasaConstrutores.kt
@@ -0,0 +1,48 @@
+package parte2_poo.classes
+
+// Utilizando construtor primário podemos:
+// - Declarar as propriedades
+// - atribuir os valores às propriedades
+class CasaComConstrutores (cor: String, vagasGaragem: Int) {
+
+    /* Utilizando construtor secundário temos que:
+    // - Declarar as propriedades
+    // - Criar os construtores separadamento
+
+    // Propriedades
+    var cor: String = ""
+    var vagasGaragem: Int = 0
+
+    // Construtor secundário 1
+    constructor(cor: String, vagasGaragem:Int) {
+        this.cor = cor
+        this.vagasGaragem = vagasGaragem
+    }
+
+    // Construtor secundário 2
+    init {
+        this.cor = cor
+        this.vagasGaragem = vagasGaragem
+    }
+    */
+
+    // Métodos
+    fun detalhesCasa() {
+        println("A casa tem a cor: $cor, vagas: $vagasGaragem")
+    }
+
+
+    fun abrirJanela(){
+        println("Abrir janelas")
+    }
+
+    fun abrirPorta(){
+        println("Abrir Porta")
+    }
+
+    fun abrirCasa(){
+        this.abrirJanela()
+        this.abrirPorta()
+    }
+
+}
