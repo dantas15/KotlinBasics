49be0b3c17222d3955d6e18ec5ecec5748399846
Gustavo Dantas Galote <gusgalote@outlook.com>
2021-05-28 18:18:47 -0300

    Basic Operators

---
+++ src/main/kotlin/parte3_operadores_controle_fluxo/OperadoresBasicos.kt
@@ -0,0 +1,17 @@
+package parte3_operadores_controle_fluxo
+
+fun main() {
+    /*
+    Somar ( + )
+    Subtrair ( - )
+    Multiplicar ( * )
+    Divisão ( / )
+    Módulo ( % )
+     */
+    var numero1 = 11
+    var numero2 = 2
+
+    var resultado = numero1 % numero2
+
+    println(resultado)
+}
